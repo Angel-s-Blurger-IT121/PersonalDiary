fc24febe458c91e9e199e78fbc3425d5ed739bd0
JackieLynCumamao <cumamao.jackielyn@gmail.com>
2026-05-01 17:36:21 +0800

    Check

--- CreateWrite.py
+++ CreateWrite.py
@@ -1,7 +1,7 @@
 def x_mode():
     try:
         file = open("Diary.txt", "x")
-        print("File Created Successfully!!")
+        print("File Created Successfully!!!")
         file.close()
     except FileExistsError:
         print("File already Exist")
