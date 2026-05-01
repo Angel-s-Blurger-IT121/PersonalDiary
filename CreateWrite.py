c2c11cee2708e21080248486a2e74fda2bcc0d34
JackieLynCumamao <cumamao.jackielyn@gmail.com>
2026-05-01 19:03:54 +0800

    a_mode

--- CreateWrite.py
+++ CreateWrite.py
@@ -5,3 +5,17 @@ def x_mode():
         file.close()
     except FileExistsError:
         print("File already Exist")
+
+def append_entry():
+    try:
+        date = input("Enter date: ")
+        entry = input("Write your diary entry: ")
+
+        file = open("diary.txt", "a")
+        file.write(f"[{date}] {entry}\n")
+        file.close()
+
+        print("Entry added!")
+
+    except Exception as e:
+        print("Error:", e)
